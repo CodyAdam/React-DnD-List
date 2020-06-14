0a38b76fadd7de5bf44f225350f32cdb702df09e
CodyAdam <cody.adam@laposte.net>
2020-06-14 14:18:37 +0200

    build

---
+++ docs/precache-manifest.95ad3dc04b411cf977f4352335b6c78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d805acbecabd4df2dc94d15e1183516",
+    "url": "./index.html"
+  },
+  {
+    "revision": "016e5f604c00672a891f",
+    "url": "./static/css/main.7827e3f3.chunk.css"
+  },
+  {
+    "revision": "10d6f884bbff549ca1ca",
+    "url": "./static/js/2.c98a5a90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c98a5a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016e5f604c00672a891f",
+    "url": "./static/js/main.3e5712ba.chunk.js"
+  },
+  {
+    "revision": "53e5b4e826605a263b82",
+    "url": "./static/js/runtime-main.67c3d358.js"
+  }
+]);
